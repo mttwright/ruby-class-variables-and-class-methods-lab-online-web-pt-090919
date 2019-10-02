3709b055476884a51c7094072e1b107538817b69
virtual-frame-7789 <mttwright@gmail.com>
2019-10-02 21:58:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,53 @@
+require "pry"
+
+
+class Song
+  
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists.push(artist)
+    @@genres.push(genre)
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres
+  end
+  
+  def self.artists
+    counter = 0
+    @@artists.collect do |artist, compare|
+      binding.pry
+    end
+  end
+  
+  def self.genre_count
+    @@genre_count
+  end
+  
+  def self.artist_count
+    @@artist_count
+  end
+  
+  
+end
+  
+  
+  
+  
+  
+    
